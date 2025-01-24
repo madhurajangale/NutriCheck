66c5a8bf6bce7b1de1ce7689e72d717e47e7ac1b
IT <madhurajangle2004@gmail.com>
2025-01-24 18:00:01 +0530

    navbar frontend

---
+++ NutriCheck/src/components/Navbar.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import '../styles/Navbar.css'
+
+const Navbar = () => {
+  return (
+    <div>
+        <div className="navbar">
+
+           <ul>
+            <li className="links"><Link to="/">Home</Link></li>
+            <li className="links"><Link to="/">About</Link></li>
+            <li className="links"><Link to="/">Community Chat</Link></li>
+            <li className="links"><Link to="/">History</Link></li>
+            <li className="links"><Link to="/">Scan</Link></li>
+            <li className="links"><Link to="/">Diet Assessment</Link></li>
+            </ul> 
+            <div className='btn-container'><button className="btn">Login</button></div>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
